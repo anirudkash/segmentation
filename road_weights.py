d51ede21909b9784fd21d0e714310b4261adc710
Anirud Kashyap <anirudk@terpmail.umd.edu>
2024-08-05 08:36:55 -0400

    add road_weights.py

---
+++ road_weights.py
@@ -0,0 +1,224 @@
+import os
+import glob
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+from unet_model import UNet, jacard_coef
+from keras.utils import normalize
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+from PIL import Image
+
+class_colors = [
+    (0, 0, 0), # background
+    (160, 150, 20), # road-flooded
+    (140, 140, 140) # road-non-flooded
+
+]
+
+background = np.array(class_colors[0])
+road_flooded = np.array(class_colors[1])
+road_nonflooded = np.array(class_colors[2])
+
+num_classes = len(class_colors)
+
+# train_imgs_direc = '/home/anirud/Desktop/SemanticSeg/train-org-img-patches'
+# train_imgs_labs_direc = '/home/anirud/Desktop/SemanticSeg/ColorMasks-TrainSet-patches'
+
+
+# -- below is one I've always been using
+# # train_imgs_direc = '/home/anirud/Desktop/SemanticSeg/TrainImgsOne'
+# # train_imgs_labs_direc = '/home/anirud/Desktop/SemanticSeg/MaskPatchesOne'
+
+train_imgs_direc = 'TrainImgsOneWithBuilds'
+train_imgs_labs_direc = 'MaskPatchesOneWithBuilds'
+
+# train_imgs_direc = '/home/anirud/Desktop/SemanticSeg/TrainImgsOne (dep)'
+# train_imgs_labs_direc = '/home/anirud/Desktop/SemanticSeg/MaskPatchesOne (dep)'
+
+#train_imgs_labs_direc = '/home/anirud/Desktop/SemanticSeg/terrain_model/terrain_only'
+
+def load_images(img_direc, gt_direc):
+
+    train_imgs = []
+    train_imgs_labs = []
+
+    train_imgs_entries = os.listdir(img_direc)
+    train_imgs_entries.sort()
+
+    train_imgs_labs_entries = os.listdir(gt_direc)
+    train_imgs_labs_entries.sort()
+
+    for fname in train_imgs_entries:
+        path = os.path.join(img_direc, fname) 
+        # might have to read in the image as gray scale 
+        img = cv2.imread(path, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (128, 128))
+        train_imgs.append(img)
+
+    for fname in train_imgs_labs_entries:
+        path = os.path.join(gt_direc, fname)
+       # img = cv2.imread(path)
+       # img = cv2.resize(img, (128, 128))
+        img = cv2.imread(path, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (128, 128))
+        train_imgs_labs.append(img)
+
+    train_images = np.array(train_imgs)
+    train_masks = np.array(train_imgs_labs)
+
+    return train_images, train_masks
+
+train_images, train_masks = load_images(train_imgs_direc, train_imgs_labs_direc)
+
+new_train_masks = np.empty_like(train_masks)
+
+for i in range(train_masks.shape[0]):
+    new_train_masks[i] = cv2.cvtColor(train_masks[i], cv2.COLOR_BGR2RGB)
+
+train_masks = new_train_masks
+
+def rgb_to_2D_label(label):
+
+    label_seg = np.zeros(label.shape,dtype=np.uint8)
+    label_seg[np.all(label==background, axis=-1)] = 0
+    label_seg[np.all(label==road_flooded, axis=-1)] = 1
+    label_seg[np.all(label==road_nonflooded, axis=-1)] = 2
+
+    label_seg = label_seg[:,:,0]
+
+    return label_seg
+
+labels = []
+for i in range(train_masks.shape[0]):
+    label = rgb_to_2D_label(train_masks[i])
+    labels.append(label)
+
+labels = np.array(labels)
+labels = np.expand_dims(labels, axis=3)
+print('Labeling done')
+
+labels_cat = to_categorical(labels, num_classes=len(class_colors))
+print('One hot encoding performed')
+X_train, X_test, y_train, y_test = train_test_split(train_images, labels_cat, test_size=0.2, random_state=42)
+
+print('Split the dataset')
+
+h = X_train.shape[1]
+w = X_train.shape[2]
+c = X_train.shape[3]
+
+model = UNet(num_classes=len(class_colors), input_size=(h,w,c))
+
+# loads in the terrain weights
+#model.load_weights('/home/anirud/Desktop/SemanticSeg/gq-weights/terrain-weights/weights_eight.weights.h5')
+
+# Loads in the road weights
+#model.load_weights('/home/anirud/Desktop/SemanticSeg/gq-weights/road-weights/revamped_two.weights.h5')
+model.load_weights('/home/anirud/Desktop/SemanticSeg/road_model/revamped_two.weights.h5')
+
+# Loads in the current weights that were learnt last
+#model.load_weights('road_new.weights.h5')
+#model.load_weights('/home/anirud/Desktop/SemanticSeg/good_weights_two.weights.h5') # used this last
+
+
+model.summary()
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.evaluate(X_test, y_test, batch_size=16, verbose=1)
+print('Evaluting model...')
+y_test_argmax = np.argmax(y_test, axis=3)
+
+patch_dir = 'alc-resized'
+gq_imgs = []
+
+patch_dirs = sorted(os.listdir(patch_dir))
+
+for curr_img in patch_dirs:
+    path = os.path.join(patch_dir, curr_img)
+    img = cv2.imread(path)
+    img = cv2.resize(img, (128,128))
+    gq_imgs.append(img)
+
+gq_imgs = np.array(gq_imgs)
+counter = 0
+for img in gq_imgs:
+    curr_img = np.expand_dims(img, axis=0)
+    predicted_img = model.predict(curr_img)
+    pixel_classes = np.argmax(predicted_img, axis=3)[0,:,:]
+    counter += 1
+
+    # plt.imshow(pixel_classes)
+    # plt.show()
+    if counter < 10:
+        counter_str = str(0)+str(0)+str(counter)
+    elif counter < 100:
+        counter_str = str(0)+str(counter)
+    else:
+        counter_str = counter
+
+    color_masks = np.zeros((pixel_classes.shape[0], pixel_classes.shape[1], 3), dtype=np.uint8)
+    
+    for class_index in range(len(class_colors)):
+        mask = pixel_classes == class_index
+        color_masks[mask] = class_colors[class_index]
+
+    cv2.imwrite(f'gq-segmented-road-classes/gq-{counter_str}.tiff', pixel_classes)
+    cv2.imwrite(f'gq-segmented-road/gq-{counter_str}.tiff', color_masks)
+
+# # patch_dir = '/home/anirud/Desktop/SemanticSeg/gq-patches/'
+# # gq_imgs = []
+
+# # patch_dirs = sorted(os.listdir(patch_dir))
+
+# # for curr_img in patch_dirs:
+# #     path = os.path.join(patch_dir, curr_img)
+# #     img = cv2.imread(path)
+# #     img = cv2.resize(img, (128, 128))
+# #     gq_imgs.append(img)
+
+
+# # gq_imgs = np.array(gq_imgs)
+# # counter = 0
+# # for img in gq_imgs:
+# #     curr_img = np.expand_dims(img, axis=0)
+# #     predicted_img = model.predict(curr_img)
+# #     pixel_classes = np.argmax(predicted_img, axis=3)[0,:,:]
+# #     counter += 1
+
+# #     # plt.imshow(pixel_classes)
+# #     # plt.show()
+# #     if counter < 10:
+# #         counter_str = str(0)+str(0)+str(counter)
+# #     elif counter < 100:
+# #         counter_str = str(0)+str(counter)
+# #     else:
+# #         counter_str = counter
+
+# #     cv2.imwrite(f'/home/anirud/Desktop/SemanticSeg/gq-segmented-terrain/gq-{counter_str}.tiff', pixel_classes)
+
+
+
+# #gq_image = cv2.imread('/home/anirud/Desktop/SemanticSeg/gq-bad.png')
+# gq_image = cv2.imread('/home/anirud/Desktop/SemanticSeg/gq-patches/gq_39.tiff')
+# print(gq_image.shape)
+# gq_image = cv2.resize(gq_image, (128, 128))
+# gq_image = np.expand_dims(gq_image, axis=0)
+# predicted_gq = model.predict(gq_image)
+# print('predicted_gq.shape: ', predicted_gq.shape)
+# print('predicted_gq: ', predicted_gq)
+# segmented_output = np.argmax(predicted_gq, axis=3)[0,:,:]
+
+# plt.figure(figsize=(12,8))
+# plt.subplot(231)
+# plt.title('Test Image')
+# plt.imshow(gq_image[0])
+# plt.subplot(232)
+# plt.title('Segmented Output')
+# plt.imshow(segmented_output)
+# plt.show()
